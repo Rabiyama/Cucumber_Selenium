b6f33ad4060effac6515dd9bd3cd770e2121f70f
Rabiyama SathickBasha <Rabiyama.SathickBatsha@genesys.com>
2019-01-17 18:17:23 +0530

    runner class updated

--- src/test/java/runners/testRunner.java
+++ src/test/java/runners/testRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features ="src/test/java/features",glue= "seleniumgluecode",
-        plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 
+        plugin = { "pretty","html:target/cucumber-reports"}, 
         monochrome = true
                 )
 
